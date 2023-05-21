f1e70919db97e8aa6449b209ab708c2ef5be20dc
Abduvokhidjon <aabduvohid33@gmail.com>
2023-05-21 12:02:28 -0500

    Task 6

---
+++ addition_assignment.js
@@ -0,0 +1,20 @@
+/*
+Task 6: Use the += operator to accumulate values in a variable
+
+Code a new variable and name it mySkills, assigning it to the value of 0.
+
+On the next line, use the += operator to increase the value of mySkills by 5.
+
+On the next line, use the += operator to increase the value of mySkills by 15.
+
+On the fourth line, console log the value of the mySkills variable.
+
+*/
+
+var mySkills
+
+mySkills = 0
+mySkills += 5
+mySkills += 15
+
+console.log(mySkills)
