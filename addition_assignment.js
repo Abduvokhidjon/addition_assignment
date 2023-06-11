84a520ca2d17d8a2d782583d4713ddbb1c930bc1
Abduvokhidjon <aabduvohid33@gmail.com>
2023-06-10 19:29:28 -0500

    Adding Assignment Function

--- addition_assignment.js
+++ addition_assignment.js
@@ -10,11 +10,16 @@ On the next line, use the += operator to increase the value of mySkills by 15.
 On the fourth line, console log the value of the mySkills variable.
 
 */
+var addingfiveintegeres = 0 
 
-var mySkills
+function addingNumbersWithAdditionAssignment(addingfiveintegeres) {
 
-mySkills = 0
-mySkills += 5
-mySkills += 15
 
-console.log(mySkills)
+  mySkills = 0;
+  mySkills += 5;
+  mySkills += 15;
+
+  return mySkills;
+}
+
+console.log(addingNumbersWithAdditionAssignment(addingfiveintegeres));
